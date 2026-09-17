1cb7d0a2e90f4ee49a07c8aef9a8dbcdc3fc136f
Martin <martin.hairer@casablanca.at>
2026-09-17 22:53:40 +0200

    1.0.1 php version upgrade

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'martin.hairer@casablanca.at',
     'author_company' => 'CASABLANCA hotelsoftware GmbH',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-14.99.99',
-            'php' => '8.1.0-8.4.99',
+            'php' => '8.1.0-8.6.99',
             'scheduler' => '',
         ],
         'conflicts' => [],
